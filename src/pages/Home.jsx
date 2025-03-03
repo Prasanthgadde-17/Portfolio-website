2727a3a82063d332f4feef582e173b69688616fd
Prasanthgadde-17 <prasanthgadde27@gmail.com>
2025-03-03 23:31:31 +0530

    update-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,79 +1,107 @@
 import img from "../assets/profile.jpg";
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
-import "./Home.css"
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaReact, FaNodeJs, FaGitAlt, FaDatabase, FaJs } from "react-icons/fa";
+import { SiPostman } from "react-icons/si";
+import "./Home.css";
 import { useNavigate } from "react-router-dom";
 
-
 function Home() {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   return (
     <div className=" Home   h-[700px]  flex items-center justify-center px-6">
       <div className=" h-full w-300  flex gap-15 items-center rounded-lg p-10 items-center">
-        
         {/* Info Section */}
-        <div className="w-[550px]   h-150  p-5 ">
+        <div className="w-[610px]   h-160  p-5 ">
           <h1 className="hname text-6xl font-bold text-gray-900 mt-20 ml-[-100px]    slide-fwd-right ">
-            Hi, I'm <span className="text-blue-600  ">Prasanth Gadde</span>
+            Hi, I`m <span className="text-blue-500">Gadde Prasanth</span>
           </h1>
-          <h3 className="text-2xl text-gray-700 mt-2 tracking-in-contract">Full Stack Web Developer</h3>
-          <p className="text-gray-600 mt-4">
-            I craft exceptional digital experiences with modern technologies
-            and a focus on user-centered design.
+          <h3 className="mt-6 text-3xl text-gray-900 mt-2 tracking-in-contract font-semobold">
+            Full Stack Web Developer
+          </h3>
+          <p className="text-gray-600 mt-4 text-lg">
+            Transforming ideas into dynamic, user-centric web experiences.
           </p>
-          
+
           {/* Buttons */}
           <div className="mt-6 flex gap-4">
-            <button onClick={()=>navigate("/contact")}  className="bg-black text-white px-6 py-2 rounded-md animate-pulse cursor-pointer">
+            <button
+              onClick={() => navigate("/contact")}
+              className="bg-black text-white px-6 py-2 rounded-md cursor-pointer buttoneffect"
+            >
               Get in Touch
             </button>
-            <button onClick={()=>navigate("/resume")} className="border border-gray-700 px-6 py-2 rounded-md cursor-pointer">
+            <button
+              onClick={() => navigate("/resume")}
+              className="border border-gray-700 px-6 py-2 rounded-md cursor-pointer bg-gray-200 buttoneffect"
+            >
               Download Resume
             </button>
           </div>
           <div className="social flex gap-6 mt-9">
-      
-      {/* LinkedIn */}
-      <a
-        href="https://www.linkedin.com/in/prasanth-gadde-fullstack/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-600 hover:text-blue-600 text-3xl transition-all duration-300 bounce-top"
-      >
-        <FaLinkedin />
-      </a>
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/prasanth-gadde-fullstack/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View my Linkedin profile"
+              className="text-gray-600 hover:text-blue-600 text-3xl transition-all duration-300 bounce-top"
+            >
+              <FaLinkedin />
+            </a>
 
-      {/* GitHub */}
-      <a
-        href="https://github.com/Prasanthgadde-17"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-600 hover:text-black text-3xl transition-all duration-300 bounce-top"
-      >
-        <FaGithub />
-      </a>
+            {/* GitHub */}
+            <a
+              href="https://github.com/Prasanthgadde-17"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="View my Github account"
+              className="text-gray-600 hover:text-black text-3xl transition-all duration-300 bounce-top"
+            >
+              <FaGithub />
+            </a>
+          </div>
 
-      {/* Twitter */}
-      <a
-        href="https://twitter.com/your-profile"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-600 hover:text-blue-400 text-3xl transition-all duration-300 bounce-top"
-      >
-        {/* <FaTwitter /> */}
-      </a>
-
-    </div>
+          <div className="tech-stack flex flex-col ">
+            <h2 className="mb-9 text-2xl font-semibold text-gray-600">
+              Technologies I Use
+            </h2>
+            <div className="icons-container">
+              <div className="tech-item">
+                <FaJs className="tech-icon" title="JavaScript (ES6+)" />
+                <span>JavaScript</span>
+              </div>
+              <div className="tech-item">
+                <FaReact className="tech-icon" title="React.js" />
+                <span>React.js</span>
+              </div>
+              <div className="tech-item">
+                <FaNodeJs className="tech-icon" title="Node.js" />
+                <span>Node.js</span>
+              </div>
+              <div className="tech-item">
+                <FaDatabase className="tech-icon" title="MongoDB" />
+                <span>MongoDB</span>
+              </div>
+              <div className="tech-item">
+                <FaGitAlt className="tech-icon" title="Git & GitHub" />
+                <span>Git & GitHub</span>
+              </div>
+              <div className="tech-item">
+                <SiPostman className="tech-icon" title="postman" />
+                <span>Postman</span>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* Profile Image Section */}
-        <div className=" w-[400px] h-[600px] ml-20 items-center mt-6">
-          <img 
-            src={img} 
-            alt="Profile" 
+        <div className=" w-[400px] h-[600px] ml-30 items-center mt-6">
+          <img
+            src={img}
+            alt="Profile"
             className="w-full h-full object-cover rounded-2xl mt-8  "
           />
         </div>
-
       </div>
     </div>
   );
